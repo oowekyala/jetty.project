1261f11e1848f0a130cc132d061e75889bbb2877
Clément Fournier <clement.fournier76@gmail.com>
2019-10-30 16:36:39 +0100

    Improve script, failing on j.net.URI::toString

--- graal/graal_compile.sh
+++ graal/graal_compile.sh
@@ -1,26 +1,57 @@
-#!/bin/sh
-# Must be run from jetty.project dir
+#!/bin/bash
+# Must be run from root dir (above jetty.project)
 
 
-SCRIPTD="$(pwd)/graal"
+JETTY_SRC_D="$(pwd)/jetty.project"
+SCRIPTD="$JETTY_SRC_D/graal"
 WD=$(mktemp -d --suffix='-jetty-graal-compile')
+
+WEBAPP_NAME="async-rest.war"
+
+APP_JAR_PATH="$WD/distribution/demo-base/webapps/$WEBAPP_NAME"
+
 
 set +e
 
-cp -r jetty-distribution/target/distribution "$WD"
+cp -r "$JETTY_SRC_D/jetty-distribution/target/distribution" "$WD"
 cp "$SCRIPTD/reflection.json" "$WD"
 
 
 pushd "$WD"
 
+# Libs extracted from war
+mkdir war-exploded
+unzip "$APP_JAR_PATH" -d war-exploded
+warlibs=$(find war-exploded -name '*.jar' | paste -sd ':' -)
+
+# Libs from jetty.home
+libs=$(find distribution/lib -name '*.jar' | paste -sd ':' -)
+
 "$GRAAL_HOME/bin/native-image" \
+    -J-Xmx4g \
+    -jar distribution/start.jar \
     --no-server \
+    --verbose \
+    --report-unsupported-elements-at-runtime \
     --enable-http \
-    --class-path lib/ \
+    --class-path "$libs:$warlibs" \
     -H:+ReportExceptionStackTraces \
-    -H:ReflectionConfigurationFiles=reflection.json \ 
-    -jar distribution/start.jar
+    "-H:ReflectionConfigurationFiles=$SCRIPTD/conf/reflect-config.json" \
+    "-H:ResourceConfigurationFiles=$SCRIPTD/conf/resource-config.json"  
+
+cp start distribution
+cd distribution
 
 
-popd "$WD"
+
+./start --create-startd
+./start --add-to-start=http,deploy
+cp "demo-base/webapps/$WEBAPP_NAME" webapps/ROOT.war
+
+
+cd webapps
+../start
+
+
+# popd "$WD"
 
